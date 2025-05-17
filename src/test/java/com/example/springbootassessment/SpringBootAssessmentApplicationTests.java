075655f4ce8a7f04aab36f22b9cf3d249e6fc13c
fabian_sellemond <sellemondfabian@gmail.com>
2025-05-17 12:04:43 +0200

    init commit

---
+++ src/test/java/com/example/springbootassessment/SpringBootAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
