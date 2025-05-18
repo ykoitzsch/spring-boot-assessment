74f4cf1334b4d31c52094b1fbcc882674e9320f5
fabian_sellemond <sellemondfabian@gmail.com>
2025-05-18 11:35:30 +0200

    setup

--- src/main/java/com/example/springbootassessment/common/web/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/springbootassessment/common/web/advice/GlobalExceptionHandler.java
@@ -1,17 +1,13 @@
 package com.example.springbootassessment.common.web.advice;
 
-import com.example.springbootassessment.project.domain.exception.ProjectNotFoundException;
-import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ProjectNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleTaskNotFound(ProjectNotFoundException ex) {
-        // todo: return ResponseEntity 404 not found with body of the given type (ProblemDetail.forStatusAndDetail)
-        throw new RuntimeException("todo: implement");
-    }
+    /*
+    add an @ExceptionHandler that returns a ResponseEntity<ProblemDetail>
+    ProblemDetail.forStatusAndDetail
+     */
+
 }
